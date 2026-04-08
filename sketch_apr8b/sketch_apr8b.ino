d8948ee7713cac656f3da5e9f700ebc11f2404ad
Renan00084 <renan00084@gmail.com>
2026-04-08 14:39:09 -0300

    Salvando codigo dos infravermelhos

---
+++ sketch_apr8b/sketch_apr8b.ino
@@ -0,0 +1,53 @@
+byte ire = A0, ird = A1;
+
+byte leituraIre, leituraIrd;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(ire,INPUT);
+  pinMode(ird,INPUT);
+  pinMode(leituraIre,INPUT);
+  pinMode(leituraIrd,INPUT);
+
+}
+
+void loop() {
+  leituraIre = analogRead(ire);
+  leituraIrd = analogRead(ird);
+    delay(1000);
+    Serial.println(leituraIre);
+    Serial.println(leituraIrd);
+
+  if(((leituraIre < 300) && (leituraIre > 100)) && ((leituraIrd < 300) && (leituraIrd > 100)) || ((leituraIre > 800) && (leituraIrd > 800))){
+    //ligar os motores no mesmo sentido
+    Serial.println("reto");
+    Serial.println(leituraIre);
+    Serial.println(leituraIrd);
+    delay(1000);
+
+  }else{
+    if(((leituraIre < 300) && (leituraIre > 100)) && (leituraIrd > 800)){
+      //curva para direita: troca o sentido das rodas da direita
+      Serial.println("direita");
+      Serial.println(leituraIre);
+      Serial.println(leituraIrd);
+      delay(1000);
+
+
+    }else{
+      if((leituraIre > 800) && ((leituraIrd < 300) && (leituraIrd > 100))){
+        //curva pra esquerda: troca o sentido das rodas da esquerda
+        Serial.println("esquerda");
+        Serial.println(leituraIre);
+        Serial.println(leituraIrd);
+        delay(1000);
+
+
+      }
+
+    }
+
+  }
+  
+
+}
