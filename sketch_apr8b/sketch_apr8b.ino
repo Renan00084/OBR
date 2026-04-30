b3c1007214694ec6660bd9eafd0ea9f1c12b0cf9
Renan00084 <renan00084@gmail.com>
2026-04-30 14:44:55 -0300

    Rodas andar

--- sketch_apr8b/sketch_apr8b.ino
+++ sketch_apr8b/sketch_apr8b.ino
@@ -3,7 +3,7 @@ byte ire = A0, ird = A1;
 byte sentido1 = 22, sentido2 = 23, enable1 = 2;
 byte sentido3 = 24, sentido4 = 25, enable2 = 3;
 byte sentido5 = 26, sentido6 = 27, enable3 = 4;
-byte sentido7 = 27, sentido8 = 28, enable4 = 5;
+byte sentido7 = 28, sentido8 = 29, enable4 = 5;
 
 byte leituraIre, leituraIrd;
 
@@ -13,15 +13,19 @@ void setup() {
   pinMode(ird,INPUT);
   pinMode(leituraIre,INPUT);
   pinMode(leituraIrd,INPUT);
+
   pinMode(sentido1, OUTPUT);
   pinMode(sentido2, OUTPUT);
   pinMode(enable1, OUTPUT);
+
   pinMode(sentido3, OUTPUT);
   pinMode(sentido4, OUTPUT);
   pinMode(enable2, OUTPUT);
+
   pinMode(sentido5, OUTPUT);
   pinMode(sentido6, OUTPUT);
   pinMode(enable3, OUTPUT);
+
   pinMode(sentido7, OUTPUT);
   pinMode(sentido8, OUTPUT);
   pinMode(enable4, OUTPUT);
@@ -31,11 +35,26 @@ void setup() {
 void loop() {
   leituraIre = analogRead(ire);
   leituraIrd = analogRead(ird);
-    delay(1000);
-    Serial.println(leituraIre);
-    Serial.println(leituraIrd);
+  Serial.println(leituraIre);
+  Serial.println(leituraIrd);
 
-  if(((leituraIre < 300) && (leituraIre > 100)) && ((leituraIrd < 300) && (leituraIrd > 100)) || ((leituraIre > 800) && (leituraIrd > 800))){
+  analogWrite(enable1, 100);
+  digitalWrite(sentido1, HIGH);
+  digitalWrite(sentido2, LOW);
+
+  digitalWrite(enable2, HIGH);
+  digitalWrite(sentido3, LOW);
+  digitalWrite(sentido4, HIGH);
+
+  digitalWrite(enable3, HIGH);
+  digitalWrite(sentido5, LOW);
+  digitalWrite(sentido6, HIGH);
+
+  analogWrite(enable4, 80);
+  digitalWrite(sentido7, HIGH);
+  digitalWrite(sentido8, LOW);
+
+  /*if(((leituraIre < 300) && (leituraIre > 100)) && ((leituraIrd < 300) && (leituraIrd > 100)) || ((leituraIre > 800) && (leituraIrd > 800))){
     //ligar os motores no mesmo sentido
     analogWrite(enable1, 100);
     digitalWrite(sentido1, HIGH);
@@ -55,7 +74,7 @@ void loop() {
 
     /*Serial.println("reto");
     Serial.println(leituraIre);
-    Serial.println(leituraIrd);*/
+    Serial.println(leituraIrd);
 
   }else{
     if(((leituraIre < 300) && (leituraIre > 100)) && (leituraIrd > 800)){
@@ -79,7 +98,7 @@ void loop() {
       /*Serial.println("direita");
       Serial.println(leituraIre);
       Serial.println(leituraIrd);
-      delay(1000);*/
+      delay(1000);
 
 
     }else{
@@ -105,14 +124,13 @@ void loop() {
         /*Serial.println("esquerda");
         Serial.println(leituraIre);
         Serial.println(leituraIrd);
-        delay(1000);*/
+        delay(1000);
 
 
       }
 
     }
 
-  }
-
+  }*/
 
 }
