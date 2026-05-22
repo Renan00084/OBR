c52b70e6a8b49feb7f9128b5ce46541e43637ddc
Renan00084 <renan00084@gmail.com>
2026-05-22 16:25:40 -0300

    Sensor de cor e motores ajustados

--- sketch_apr8b/sketch_apr8b.ino
+++ sketch_apr8b/sketch_apr8b.ino
@@ -1,8 +1,10 @@
+void verde();
+
 byte ire = 31, ird = 30; //31 fio branco e 32 fio cinza
 
 /*byte ire = A0, ird = A1;*/
 
-byte led = 32, led2 = 33;
+byte OUTe = 34, S2e = 32, S3e = 33, pulseRe = 0, pulseGe = 0, pulseBe = 0;
 
 byte sentido1 = 22, sentido2 = 23, enable1 = 2;
 byte sentido3 = 24, sentido4 = 25, enable2 = 3;
@@ -11,7 +13,6 @@ byte sentido7 = 28, sentido8 = 29, enable4 = 5;
 byte leituraIre, leituraIrd;
 
 void setup() {
-  Serial.begin(9600);
   pinMode(ire,INPUT);
   pinMode(ird,INPUT);
   pinMode(leituraIre,INPUT);
@@ -33,15 +34,14 @@ void setup() {
   pinMode(sentido8, OUTPUT);
   pinMode(enable4, OUTPUT);
 
-  pinMode(led, OUTPUT);
-  pinMode(led2, OUTPUT);
+  pinMode(S2e, OUTPUT);
+  pinMode(S3e, OUTPUT);
+  pinMode(OUTe, INPUT);
+  Serial.begin(115200);
 
 }
 //Branco: entre 0 e 50/ Preto: maior que 100 / Verde: entra 50 e 100
 void loop() {
-  digitalWrite(led, HIGH);
-  digitalWrite(led2, HIGH);
-
   leituraIre = digitalRead(ire);
   leituraIrd = digitalRead(ird);
   Serial.println(leituraIre);
@@ -49,6 +49,9 @@ void loop() {
 
   if(((leituraIre == HIGH) && (leituraIrd == HIGH)) || ((leituraIre == LOW) && (leituraIrd == LOW))){
     //reto
+
+    verde();
+
     analogWrite(enable1, 100); // Esquerda Frente
     digitalWrite(sentido1, HIGH);
     digitalWrite(sentido2, LOW);
@@ -68,6 +71,8 @@ void loop() {
   }else{
     if(((leituraIre == HIGH) && (leituraIrd == LOW))){
       //curva esquerda
+
+      verde();
 
       analogWrite(enable1, 255); // Esquerda Frente
       digitalWrite(sentido1, LOW);
@@ -89,6 +94,8 @@ void loop() {
       if(((leituraIre == LOW) && (leituraIrd == HIGH))){
         //curva direita
 
+        verde();
+
         analogWrite(enable1, 0); // Esquerda Frente
         digitalWrite(sentido1, HIGH);
         digitalWrite(sentido2, LOW);
@@ -108,85 +115,67 @@ void loop() {
       }
     }
   }
-// Branco direita: entre 90 e 100/ esquerda: 
-  /*leituraIre = analogRead(ire);
-  leituraIrd = analogRead(ird);
-  Serial.print("Esquerda: ");
-  Serial.println(leituraIre);
-  Serial.print("Direita: ");
-  Serial.println(leituraIrd);
+}
 
-  digitalWrite(led, HIGH);
-  digitalWrite(led2, HIGH);
+void verde(){
+  //Seleciona leitura com filtro para vermelho
+  digitalWrite(S2e,LOW);
+  digitalWrite(S3e,LOW);
+  //Lê duração do pulso em LOW
+  pulseRe = pulseIn(OUTe, LOW);
+  //Imprime via serial
+  Serial.print(" RED = ");
+  Serial.print(pulseRe);
+  Serial.print(" | ");
+  delay(20);
 
+  //Seleciona leitura com filtro para verde
+  digitalWrite(S2e,HIGH);
+  digitalWrite(S3e,HIGH);
+  //Lê duração do pulso em LOW
+  pulseGe = pulseIn(OUTe, LOW);
+  //Imprime via serial
+  Serial.print("GREEN = ");
+  Serial.print(pulseGe);
+  Serial.print(" | ");
+  delay(20);
+  
 
-  if(((leituraIre < 100) && (leituraIrd < 100)) || ((leituraIre > 100) && (leituraIrd > 100))){
-    //reto
-    analogWrite(enable1, 100); // Esquerda Frente
-    digitalWrite(sentido1, HIGH);
-    digitalWrite(sentido2, LOW);
+  //Seleciona leitura com filtro para azul
+  digitalWrite(S2e, LOW);
+  digitalWrite(S3e,HIGH);
+  //Lê duração do pulso em LOW
+  pulseBe = pulseIn(OUTe, LOW);
+  //Imprime via serial
+  Serial.print("BLUE = ");
+  Serial.println(pulseBe);
+  delay(20);
 
-    digitalWrite(enable2, HIGH); // Esquerda Atras
-    digitalWrite(sentido3, LOW);
-    digitalWrite(sentido4, HIGH);
+  if((pulseGe < pulseRe) && (pulseGe < pulseBe)){
+    for(byte i = 0; i < 300; i++){
+      analogWrite(enable1, 100); // Esquerda Frente
+      digitalWrite(sentido1, HIGH);
+      digitalWrite(sentido2, LOW);
 
-    digitalWrite(enable3, HIGH); // Direita Atras
-    digitalWrite(sentido5, LOW);
-    digitalWrite(sentido6, HIGH);
+      analogWrite(enable2, 100); // Esquerda Atras
+      digitalWrite(sentido3, LOW);
+      digitalWrite(sentido4, HIGH);
 
-    analogWrite(enable4, 100); // Direita Frente
-    digitalWrite(sentido7, LOW);
-    digitalWrite(sentido8, HIGH);
+      analogWrite(enable3, 100); // Direita Atras
+      digitalWrite(sentido5, HIGH);
+      digitalWrite(sentido6, LOW);
 
-  }else{
-    if((leituraIre > 100) && (leituraIrd < 100)){
-      //curva esquerda
-      for(byte i=0; i<200; i--){
-        analogWrite(enable1, 100); // Esquerda Frente
-        digitalWrite(sentido1, LOW);
-        digitalWrite(sentido2, HIGH);
+      analogWrite(enable4, 100); // Direita Frente
+      digitalWrite(sentido7, HIGH);
+      digitalWrite(sentido8, LOW);
 
-        digitalWrite(enable2, HIGH); // Esquerda Atras
-        digitalWrite(sentido3, HIGH);
-        digitalWrite(sentido4, LOW);
-
-        digitalWrite(enable3, LOW); // Direita Atras
-        digitalWrite(sentido5, LOW);
-        digitalWrite(sentido6, HIGH);
-
-        analogWrite(enable4, 0); // Direita Frente
-        digitalWrite(sentido7, HIGH);
-        digitalWrite(sentido8, LOW);
-
-      }
-
-    }else{
-      if((leituraIre < 100) && (leituraIrd > 100)){
-        //curva direita
-        for(byte i=0; i<200; i--){
-          analogWrite(enable1, 0); // Esquerda Frente
-          digitalWrite(sentido1, HIGH);
-          digitalWrite(sentido2, LOW);
-
-          digitalWrite(enable2, LOW); // Esquerda Atras
-          digitalWrite(sentido3, LOW);
-          digitalWrite(sentido4, HIGH);
-
-          digitalWrite(enable3, HIGH); // Direita Atras
-          digitalWrite(sentido5, HIGH);
-          digitalWrite(sentido6, LOW);
-
-          analogWrite(enable4, 100); // Direita Frente
-          digitalWrite(sentido7, HIGH);
-          digitalWrite(sentido8, LOW);
-        }
+      if(i == 200){
+        break;
 
       }
 
     }
 
-  }*/
-
-  // Ambos os sensores ficam high quando idenifica preto
+  }
 
 }
